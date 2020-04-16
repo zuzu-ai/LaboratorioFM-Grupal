c7b584d441fd9e712ca64f8c5adc0223a6383ce2
zuzu-ai <sucelyalvarez@hotmail.com>
2020-04-16 10:54:36 -0600

    reparacion

---
+++ Base De Datos.sql
@@ -0,0 +1,77 @@
+create database LabFM;
+use  LabFM;
+
+create table Clientes(
+Tarjeta_C int primary key auto_increment,
+Nombre_C varchar(60) not null,
+Dpi_C int not null,
+Tel_C int not null,
+Direccion_C varchar(60) not null,
+Correo_C varchar(60) not null,
+Mora_Acum decimal(10,2) not null,
+Renta_Acum decimal(10,2) not null,
+Bonos_Acum int not null
+)engine=innodb;
+
+create table Peliculas(
+Codigo_P int primary key auto_increment,
+Nombre_P varchar(60) not null,
+Autor_P varchar(60) not null,
+Genero_P varchar(60) not null,
+Clasificacion_P varchar(60) not null,
+Duracion_P int not null,
+Precio_P decimal(10,2) not null,
+Existencia int not null,
+Tipo_A varchar(1) default'P'
+)engine=Innodb;
+
+create table Juegos(
+Codigo_V int primary key auto_increment,
+Nombre_V varchar(60) not null,
+Genero_V varchar(60) not null,
+Precio_V decimal(10,2) not null,
+Existencia int not null,
+Tipo_A varchar(1) default'V'
+)engine=innodb;
+
+create table Empleados(
+Codigo_E int primary key auto_increment,
+Nombre_E varchar(60) not null,
+Estado_E varchar(1) not null
+)Engine=Innodb;
+
+create table Rentas(
+Codigo_R int primary key auto_increment,
+Tarjeta_C int not null,
+Codigo_A int not null,
+Cantidad_A int not null,
+Total_R decimal(10,2) not null,
+Mora decimal(10,2) not null,
+Fecha_Inico varchar(10) not null,
+Fecha_Vencimiento varchar(10) not null,
+
+foreign key (Tarjeta_C) references
+Clientes(Tarjeta_C)
+)engine=Innodb;
+
+create table Devoluciones(
+Codigo_D int primary key,
+Codigo_R int not null,
+Fecha_D datetime not null,
+
+foreign key (Codigo_R) references
+Rentas(Codigo_R)
+)engine=Innodb;
+
+create table Tiendas(
+Codigo_T int primary key auto_increment,
+Nombre_T varchar(60) not null,
+Ubicacion_T varchar(60) not null,
+Estado varchar(1) not null,
+Codigo_E int not null,
+
+foreign key (Codigo_E) references
+Empleados(Codigo_E)
+)engine=Innodb;
+select * from peliculas;
+drop database LabFM;
